36919e33a5d91d8d4166f13f06e1fb668a55c339
luchoaglp <lucho_aglp@hotmail.com>
2019-03-30 18:52:26 -0300

    first commit

---
+++ src/main/java/ar/com/vendepor/vendepor48/VendePor48Application.java
@@ -0,0 +1,13 @@
+package ar.com.vendepor.vendepor48;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendePor48Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendePor48Application.class, args);
+	}
+
+}
