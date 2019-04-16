78d475eef25bda5a0e686c5539d145fd37b87b4e
luchoaglp <lucho_aglp@hotmail.com>
2019-04-16 14:34:46 -0300

    Updated ws.js

--- src/main/resources/static/js/ws.js
+++ src/main/resources/static/js/ws.js
@@ -1,30 +1,36 @@
+function connect() {
+    let socket = new SockJS('/ws');
+    const stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+    console.log('Connected: ' + frame);
+    stompClient.subscribe('/queue/reply', function (msg) {
+    const message = JSON.parse(msg.body);
+        const dateTime = new Date(message.messageDateTime);
+        const dateTimeStr = `${dateTime.getFullYear()}/${dateTime.getMonth() + 1}/${dateTime.getDate()} ${dateTime.getHours()}:${dateTime.getMinutes()}:${dateTime.getSeconds()}`;
+
+        console.log('WS', message);
+        $messages.prepend(
+            `<li class="list-group-item">
+                [<b>${dateTimeStr}</b>]
+                <a href="#">${message.client.username}</a>
+                ${message.message}
+                </li>`);
+        });
+    });
+    return stompClient;
+}
+
+function disconnect(stompClient) {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    console.log("Disconnected");
+}
+
 $(function() {
 
-    let stompClient = null;
+    let stompClient = connect();
     const $messages = $('#messages');
-
-    function connect() {
-        let socket = new SockJS('/ws');
-        stompClient = Stomp.over(socket);
-        stompClient.connect({}, function (frame) {
-            console.log('Connected: ' + frame);
-            stompClient.subscribe('/queue/reply', function (msg) {
-                const message = JSON.parse(msg.body);
-                const dateTime = new Date(message.messageDateTime);
-                const dateTimeStr = `${dateTime.getFullYear()}/${dateTime.getMonth() + 1}/${dateTime.getDate()} ${dateTime.getHours()}:${dateTime.getMinutes()}:${dateTime.getSeconds()}`;
-
-                console.log('WS', message);
-                $messages.prepend(
-                    `<li class="list-group-item">
-                        [<b>${dateTimeStr}</b>]
-                        <a href="#">${message.client.username}</a>
-                        ${message.message}
-                    </li>`);
-            });
-        });
-    }
-
-    connect();
 
     $('#send').click(() => {
 
@@ -40,5 +46,10 @@ $(function() {
         });
 
     });
+
+    $(window).bind('beforeunload', function() {
+        disconnect(stompClient);
+    });
+
 
 });
