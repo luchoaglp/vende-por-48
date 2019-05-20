c8a6aed28a62e2dc7afa63193f8fb8b7d411fe46
luchoaglp <lucho_aglp@hotmail.com>
2019-05-20 12:36:48 -0300

    Added liked to JS

--- src/main/resources/static/js/publication.js
+++ src/main/resources/static/js/publication.js
@@ -5,18 +5,25 @@ $(function() {
     let messages = [];
     let stompClient = null;
 
-    $.get("/publication/messages/1", function(data) {
-        messages = data;
-        messages.forEach(message => {
+    const publication = window.location.pathname.split("/");
+    const publicationId = publication[publication.length - 1];
+
+    function displayMsg(message) {
         const dateTime = new Date(message.messageDateTime);
         const dateTimeStr = `${dateTime.getFullYear()}/${dateTime.getMonth() + 1}/${dateTime.getDate()} ${dateTime.getHours()}:${dateTime.getMinutes()}:${dateTime.getSeconds()}`;
+        const liked = message.liked ? '<span class="text-success font-weight-bold">✓</span>' : '';
         $messages.append(
             `<li class="list-group-item">
                 [<b>${dateTimeStr}</b>]
                 <a href="#">${message.client.username}</a>:
                 ${message.message}
+                ${liked}
             </li>`);
-        });
+    }
+
+    $.get("/publication/messages/" + publicationId, function(data) {
+        messages = data;
+        messages.forEach(message => displayMsg(message));
 
     }).fail(function() {
         console.log("Error");
@@ -28,17 +35,8 @@ $(function() {
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
             stompClient.subscribe('/queue/reply', function (msg) {
-            const message = JSON.parse(msg.body);
-            const dateTime = new Date(message.messageDateTime);
-            const dateTimeStr = `${dateTime.getFullYear()}/${dateTime.getMonth() + 1}/${dateTime.getDate()} ${dateTime.getHours()}:${dateTime.getMinutes()}:${dateTime.getSeconds()}`;
-
-            console.log('WS', message);
-            $messages.prepend(
-                `<li class="list-group-item">
-                    [<b>${dateTimeStr}</b>]
-                    <a href="#">${message.client.username}</a>:
-                    ${message.message}
-                </li>`);
+                const message = JSON.parse(msg.body);
+                displayMsg(message);
             });
         });
     }
@@ -49,7 +47,7 @@ $(function() {
 
         $.ajax({
             method: "POST",
-            url: `/publication/message/${$("#publicationId").val()}`,
+            url: `/publication/message/${publicationId}`,
             contentType: "application/json; charset=utf-8",
             dataType: 'json',
             data: JSON.stringify({ message: $.trim($('#msg').val()) })
